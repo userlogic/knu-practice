5039c6d82b8d3cb2daf39b092f05a272487e8dc0
userlogic <45264389+userlogic@users.noreply.github.com>
2018-11-28 12:03:28 +0900

    Revert "덧셈 기능 추가"

--- adder.c
+++
@@ -1,5 +0,0 @@
-int adder(int a, int b) {
-	printf("kdlajldjalf");
-	printf("dfkdjfkdj");
-	return a + b;
-}
--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main(void) {
-	int n, m;
+	int n;
 
-	scanf("%d %d", &n, &m);
-	//printf("%d\n", n);
+	scanf("%d", &n);
+	printf("%d\n", n);
 	return 0;
 }
